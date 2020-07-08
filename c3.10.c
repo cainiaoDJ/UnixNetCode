77363e1fe9595df4d480b1abb51760e413dfbe3c
root <root@192-168-115-134.localdomain>
2020-07-08 16:11:48 +0800

    测试系统是大端还是小端序

---
+++ c3.10.c
@@ -0,0 +1,31 @@
+#include "unp.h"
+int main(int argc, char **argv)
+{
+    union
+    {
+        short a;
+        char c[sizeof(short)];
+    } un;
+    un.a = 0x102;
+    printf("%s: ", CPU_VENDOR_OS);
+    if (sizeof(short) == 2)
+    {
+        if (un.c[0] == 1 && un.c[1] == 2)
+        {
+            printf("big-endian\n");
+        }
+        else if (un.c[1] == 1 && un.c[0] == 2)
+        {
+            printf("little-endian\n");
+        }
+        else
+        {
+            printf("unknown\n");
+        }
+    }
+    else
+    {
+        printf("sizeof(short) = %d \n", sizeof(short));
+    }
+    exit(0);
+}
